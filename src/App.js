ca57987f4d1d169f8c36cd4f3f21df9be4178737
“Iran <iranjunior94@gmail.com>
2020-05-16 15:11:50 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
+import io from 'socket.io-client';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const socket = io('http://localhost:3333');
+  
+  socket.on('recivedMessage', (data) => {
+    const messages = document.querySelector('#messages');
+    console.log('data:', data)
+    messages.innerHTML = `<span><strong>${data.author}: </strong>${data.message}</span>`;
+  });
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = document.querySelector('#name').value;
+    const message = document.querySelector('#message').value;
+    console.log('entrouaqui', {name, message})
+    socket.emit('sendMessage', {
+      author: name,
+      message,
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="messages"></div>
+    <form>
+      <input id="name" placeholder="Digite seu nome" />
+      <button type="submit" onClick={handleSubmit}>
+        Enviar
+      </button>
+      <input id="message" placeholder="Digite sua message" multiple />
+    </form>
     </div>
   );
 }
